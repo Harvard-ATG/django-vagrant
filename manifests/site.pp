28649e99643d11e10fccbd5d38457da66df9f6a5
Arthur Barrett <abarrett@fas.harvard.edu>
2014-04-21 16:57:16 -0400

    add dependencies for python Pillow module and create a django super user

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,3 @@
-$PROJ_DIR = "/home/vagrant/HarvardCards"
-$GIT_CLONE_URL = "https://github.com/Harvard-ATG/HarvardCards.git"
-
 Exec {
 	path => "/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin",
 }
@@ -21,27 +18,41 @@ class init {
 		require => Exec['update-apt'],
 	}
 
+	# install some dependencies
+	package {
+		["python", "python-dev", "python-pip", "python-setuptools"]:
+		ensure => installed,
+		require => Exec['update-apt']
+	}
+}
+
+class djangoapp {
+	$PROJ_DIR = "/home/vagrant/HarvardCards"
+	$GIT_CLONE_URL = "https://github.com/Harvard-ATG/HarvardCards.git"
+
+	# install dependencies needed for Pillow (python imaging module)
+	# https://pypi.python.org/pypi/Pillow/
+	# http://pillow.readthedocs.org/
+	package { 
+		['libtiff4-dev', 'libjpeg8-dev', 'zlib1g-dev',  'libfreetype6-dev', 'liblcms2-dev', 'tcl8.5-dev', 'tk8.5-dev', 'python-tk']:
+		ensure => installed,
+		require => Exec['update-apt']
+	}
+
 	# get application from git
-	exec { 'git-app':
+	exec { 'git-clone':
 		command => "git clone $GIT_CLONE_URL $PROJ_DIR",
 		require => Package['git'],
 		onlyif => ["test ! -d $PROJ_DIR/.git"],
 		logoutput => true,
 	}
 
-	# install some dependencies
-	package {
-		["python", "python-dev", "python-pip"]:
-		ensure => installed,
-		require => Exec['update-apt']
-	}
-	
 	# install project dependencies
 	exec { "pip-install-requirements":
 		command => "sudo /usr/bin/pip install -r $PROJ_DIR/requirements.txt",
 		tries => 2,
 		timeout => 600,
-		require => [Package['python-pip', 'python-dev'],Exec['git-app']],
+		require => [Package['python-pip', 'python-dev'],Exec['git-clone']],
 		logoutput => true,
 	}
 
@@ -53,12 +64,20 @@ class init {
 		logoutput => true,
 	}
 
+	# setup super user
+	exec { "django-setup-superuser":
+		command => 'echo "from django.contrib.auth.models import User; User.objects.create_superuser(\'admin\', \'admin@example.com\', \'admin\')" | ./manage.py shell',
+		cwd => "$PROJ_DIR",
+		require => Exec['django-syncdb']
+	}
+
 	# start server?
 	exec { "django-runserver":
-		command => "python manage.py runserver 0.0.0.0:8000 2>&1 server.log &",
+		command => "python manage.py runserver 0.0.0.0:8000 > server.log &",
 		cwd => "$PROJ_DIR",
 		require => Exec['django-syncdb'],
 		logoutput => true,	
 	}
-
 }
+
+include djangoapp
